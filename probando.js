b44bf1a18b69eb6ecad38f48e58e21714e519a43
Marcos <marcosportomene@hotmail.com>
2023-02-22 19:39:55 -0300

    primer commit

---
+++ probando.js
@@ -0,0 +1,8 @@
+
+let edad = 21
+
+let nombre = "marcos" 
+
+console.log( edad )
+
+console.log( nombre )
---
+++ variables.js
@@ -0,0 +1,36 @@
+
+//VARIABLES -- Caja que guarda algo
+//Palabras reservadas(palabras que tienen un significado para js)
+
+let nombreDeUsuario = "jorge duje"
+//Diferencia entre let y const; let puede ser modificada despues de crearla; const es una constante, es decir que no puede ser modificada despues de crearla.
+// const apellido = "Perez"
+
+    nombreDeUsuario = "Pepito"
+
+console.log( nombreDeUsuario )
+
+
+//TIPOS DE DATOS
+
+// strings = cadena de caracteres o cadena de texto (texto entre comillas simples ' o comillas dobles ")
+// number = numeros, simplemente numeros(no poner comillas porque deja de ser un number y pasa a ser un string) pueden dividirse entre inter o flow(numeros enteros = inter o decimales = flow)
+// Booleanos o boolean = si o no, true or false.
+// undefined = variable que no tiene valor aun, temporalmente.
+//null = variable que no tiene contenido nunca
+//NaN = "Not a number" operacion sin numeros
+
+
+//OPERADORES
+
+let nombre = "juancito"
+let apellido = "lopez"
+
+let nombreCompleto = nombre + " " + apellido
+
+console.log( nombreCompleto )
+
+
+//OPERADORES DE COMPARACION
+
+//OPERADORES LOGICOS
